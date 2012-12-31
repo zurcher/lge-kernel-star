1ea9489e223e94c6fb0105be47f83007c06c54ca
J. Anderson <janderson@janderson.ca>
2012-12-30 22:33:11 -0500

    Try GB values again with dual SPI compat code.... Plus a debug line.

--- drivers/spi/spi_mdm6600.c
+++ drivers/spi/spi_mdm6600.c
@@ -1565,6 +1565,7 @@ ifx_spi_tty_callback( struct ifx_spi_data *spi_data)
 }
 
 static void ifx_spi_complete(void *arg) {
+    printk(KERN_INF) "mdm6600: ifx_spi_complete called");
     complete(arg);
 }
 
